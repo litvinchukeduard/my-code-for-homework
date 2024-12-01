78f087dc68a62d04fb91d33616e91c8ffd10bc37
Eduard Litvinchuk <litvinchuckeduard@gmail.com>
2024-12-01 19:17:04 +0200

    Functions to check odd numbers

--- debugger_example.py
+++ debugger_example.py
@@ -5,8 +5,21 @@
 лише непарні числа
 '''
 
+def is_odd(number: int) -> bool:
+    '''
+    Function that receives a number and returns if it is odd
+    '''
+    # if number % 2 != 0:
+    #     return True
+    # else:
+    #     return False
+    return number % 2 != 0
+
 #print_odd_numbers
-def print_only_odd_numbers_in_a_list(number_list):
+def print_odd_numbers(number_list: list[int]):
+    '''
+    A function that receives a list of numbers and prints only odd numbers 
+    '''
     # Пройтися по усім числам
     for number in number_list:
         # Перевірити чи конкретне число є парне чи ні
@@ -25,8 +38,12 @@ def print_only_odd_numbers_in_a_list(number_list):
 # To clear terminal:
 # Ctrl L 
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-print_only_odd_numbers_in_a_list(numbers)
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print_odd_numbers(numbers)
+
+print_odd_numbers({1, 2, 3, 4, 5})
+
+# print(is_odd("Hello"))
 
 # 2 4 6 8 10
 # 3 5 7 9 11
